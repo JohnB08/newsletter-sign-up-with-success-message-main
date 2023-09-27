4a7472f1e2339b4cc0486fb19605d140e606c30d
JohnB08 <johnb08.kodehode@gmail.com>
2023-09-27 12:04:24 +0200

    comments

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const subPage = document.getElementById("submitpage");
 const returnBtn = document.getElementById("dismissbtn");
 const error = document.getElementById("error");
 let emailValid = document.getElementById("emailinput");
+/* I still don't know any better ways to fetch elements from my HTML
+like fetching the css sheet and styling that instead of inline css. */
 subBtn.addEventListener("click", (event) => {
   if (email.value && email.value.includes("@" && ".")) {
+    //This worked quite nicely. Probably not an ideal way to validate emails.
     mainPage.style.display = "none";
     subPage.style.display = "grid";
     emailValid.textContent = email.value;
