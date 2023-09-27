05834d6eced02e63e08a72949559cc72776ed4f9
JohnB08 <johnb08.kodehode@gmail.com>
2023-09-27 11:46:14 +0200

    trying to get the JS working

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const mainPage = document.getElementById("maincontent");
 const email = document.getElementById("Emailfield");
 const subPage = document.getElementById("submitpage");
 const returnBtn = document.getElementById("dismissbtn");
+const error = document.getElementById("error");
+let emailValid = document.getElementById("emailinput");
 subBtn.addEventListener("click", (event) => {
-  if (email.value) {
+  if (email.value && email.value.includes("@" && ".")) {
     mainPage.style.display = "none";
     subPage.style.display = "grid";
+    emailValid.textContent = email.value;
+  } else {
+    error.style.display = "inline";
+    email.style.backgroundColor = "hsla(4, 100%, 67%, 0.5)";
+    email.style.borderColor = "hsl(4, 100%, 67%)";
   }
 });
 returnBtn.addEventListener("click", (event) => {
